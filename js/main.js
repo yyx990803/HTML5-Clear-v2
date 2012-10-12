eb4773c4807657cdabcd036afa514c27fde18784
Evan You <yyou@google.com>
2012-10-12 17:18:38 -0400

    theme

--- js/main.js
+++ js/main.js
@@ -16,9 +16,27 @@ var C = {
 
 	init: function () {
 
-		this.client.init();
-		this.db.init();
-		this.menu.init();
+		C.client.init();
+		C.db.init();
+		C.menu.init();
+
+		C.setTheme(C.db.data.theme, true);
+
+	},
+
+	setTheme: function (newTheme, force) {
+
+		var curTheme = C.db.data.theme;
+		if (newTheme == curTheme && !force) return;
+
+		C.$wrapper
+			.removeClass(curTheme)
+			.addClass(newTheme);
+
+		C.db.data.theme = newTheme;
+		C.db.save();
+
+		C.log('Set theme: ' + newTheme);
 
 	},
 
