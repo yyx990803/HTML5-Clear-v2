5a3c10801a332905df5d2e34dce041a64526ee80
Evan You <yyx990803@gmail.com>
2012-10-14 00:10:51 -0400

    test build script

--- js/main.js
+++ js/main.js
@@ -1,91 +1,87 @@
-;(function () {
+var C = {
 
-	window.C = {
+	debug: true,
 
-		debug: true,
+	// dom elements
+	$wrapper: 		$('#wrapper'),
 
-		// dom elements
-		$wrapper: 		$('#wrapper'),
+	// states
+	states: {
+		MENU: 		0,
+		LISTS: 		1,
+		TODOS: 		2,
+		EDITING: 	3
+	},
 
-		// states
-		states: {
-			MENU: 		0,
-			LISTS: 		1,
-			TODOS: 		2,
-			EDITING: 	3
-		},
+	init: function () {
 
-		init: function () {
+		C.start = new Date().getTime();
 
-			C.start = new Date().getTime();
+		// init some components
+		C.client.init();
+		C.db.init();
+		C.touch.init();
 
-			// init some components
-			C.client.init();
-			C.db.init();
-			C.touch.init();
+		C.menu.init();
+		C.listView.init();
 
-			C.menu.init();
-			C.listView.init();
+		// set theme
+		C.$wrapper.addClass(C.db.data.theme);
 
-			// set theme
-			C.$wrapper.addClass(C.db.data.theme);
+		// restore state
+		var state = C.db.data.state;
+		switch (state.view) {
 
-			// restore state
-			var state = C.db.data.state;
-			switch (state.view) {
+			case C.states.MENU:
+				C.log('App: init at menu.');
+				C.currentView = C.menu;
+				break;
 
-				case C.states.MENU:
-					C.log('App: init at menu.');
-					C.currentView = C.menu;
-					break;
+			case C.states.LISTS:
+				C.log('App: init at lists.');
+				C.currentView = C.listView;
+				break;
 
-				case C.states.LISTS:
-					C.log('App: init at lists.');
-					C.currentView = C.listView;
-					break;
+			case C.states.TODOS:
+				C.log('App: init at list with ID: ' + state.listID);
+				C.currentView = new C.TodoList(C.db.data.lists[state.listID]);
+				break;
 
-				case C.states.TODOS:
-					C.log('App: init at list with ID: ' + state.listID);
-					C.currentView = new C.TodoList(C.db.data.lists[state.listID]);
-					break;
-
-				default:
-					C.log('App: init at lists.');
-					C.currentView = C.listView;
-					break;
-
-			}
-
-			C.$wrapper.append(C.currentView.el);
-
-		},
-
-		setTheme: function (newTheme) {
-
-			var curTheme = C.db.data.theme;
-			if (newTheme == curTheme) return;
-
-			C.$wrapper
-				.removeClass(curTheme)
-				.addClass(newTheme);
-
-			C.db.data.theme = newTheme;
-			C.db.save();
-
-			C.log('App: set theme: ' + newTheme);
-
-		},
-
-		log: function (msg) {
-
-			msg = '[' + (new Date().getTime() - C.start) + 'ms] ' + msg;
-			if (this.debug) console.log(msg);
+			default:
+				C.log('App: init at lists.');
+				C.currentView = C.listView;
+				break;
 
 		}
 
-	};
+		C.$wrapper.append(C.currentView.el);
 
-}());
+	},
+
+	setTheme: function (newTheme) {
+
+		var curTheme = C.db.data.theme;
+		if (newTheme == curTheme) return;
+
+		C.$wrapper
+			.removeClass(curTheme)
+			.addClass(newTheme);
+
+		C.db.data.theme = newTheme;
+		C.db.save();
+
+		C.log('App: set theme: ' + newTheme);
+
+	},
+
+	log: function (msg) {
+
+		msg = '[' + (new Date().getTime() - C.start) + 'ms] ' + msg;
+		if (this.debug) console.log(msg);
+
+	}
+
+};
 
 $(function () {
 	C.init();
