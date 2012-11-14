0889f914904dd0067ed6f4ad54bec45c83d895ed
Evan You <yyx990803@gmail.com>
2012-11-14 00:20:40 -0500

    build script remove menu.js

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ BASEDIR=$(dirname $0)
 
 #copy stuff to deploy folder
 
-echo -e "\033[36m[update]\033[0m copying assets into deploy folder..."
+echo "\033[36m[update]\033[0m copying assets into deploy folder..."
 rm -rf $BASEDIR/deploy
 mkdir $BASEDIR/deploy
 mkdir $BASEDIR/deploy/js
@@ -15,7 +15,7 @@ cp $BASEDIR/index.html $BASEDIR/deploy/index.html
 
 #rewrite index.html for min.js
 
-echo -e "\033[32m[minify]\033[0m rewriting script tag in" $BASEDIR"/deploy/templates/index.html"
+echo "\033[32m[minify]\033[0m rewriting script tag in" $BASEDIR"/deploy/templates/index.html"
 #1. remove all current scripts
 sed -i.bak '/<script.*\/script>/d' $BASEDIR/deploy/index.html
 #2. replace <!-- MIN --> tag with min.js
@@ -23,14 +23,13 @@ sed -i.bak 's/<!-- MIN -->/<script src="js\/min.js"><\/script>/' $BASEDIR/deploy
 
 #minify javascript with uglifyjs
 
-echo -e "\033[32m[minify]\033[0m source:" $BASEDIR"/js/"
-echo -e "\033[32m[minify]\033[0m output:" $BASEDIR"/deploy/js/min.js"
+echo "\033[32m[minify]\033[0m source:" $BASEDIR"/js/"
+echo "\033[32m[minify]\033[0m output:" $BASEDIR"/deploy/js/min.js"
 cat $BASEDIR/js/zepto.js \
 $BASEDIR/js/main.js \
 $BASEDIR/js/client.js \
 $BASEDIR/js/db.js \
 $BASEDIR/js/touch.js \
-$BASEDIR/js/menu.js \
 $BASEDIR/js/item.js \
 $BASEDIR/js/collection.js \
 $BASEDIR/js/list-collection.js \
@@ -42,11 +41,11 @@ $BASEDIR/js/todo-item.js \
 #remove stuff no longer needed for deploy
 
 #1. remove index.html backup
-echo -e "\033[33m[cleanup]\033[0m removing index.html backup..."
+echo "\033[33m[cleanup]\033[0m removing index.html backup..."
 rm $BASEDIR/deploy/index.html.bak
 
 #2. remove .DS_Store (if any)
-echo -e "\033[33m[cleanup]\033[0m removing .DS_Store if any..."
+echo "\033[33m[cleanup]\033[0m removing .DS_Store if any..."
 find $BASEDIR/deploy -name .DS_Store -exec rm {} \;
 
-echo -e "\033[31m[done]\033[0m"
+echo "\033[31m[done]\033[0m"
